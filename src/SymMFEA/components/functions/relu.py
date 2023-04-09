add loga and fix derivative of relu

src/SymMFEA/components/functions/relu.py
@@ -19,8 +19,8 @@ class Relu(Node):
         
         #calculate d
         self.dW = out
-        self.dX =  np.expand_dims(np.maximum(operands[0], 0) * self.value, axis =0)
-        assert self.dX.ndim == 2
+        self.dX =  np.where(operands > 0, 1.0, 0.0)
+        assert self.dX.ndim == 2, self.dX.ndim
         return out
     
     

src/SymMFEA/components/functions/subtract.py
@@ -27,6 +27,6 @@ class Subtract(Node):
         self.dX = np.full((operands.shape[0], operands.shape[1]), -self.value, np.float64)
         self.dX[0] = -self.dX[0]
         
-        assert self.dX.ndim == 2
+        assert self.dX.ndim == 2, self.dX.ndim
         return out 
         

src/SymMFEA/components/functions/sum.py
@@ -18,5 +18,5 @@ class Sum(Node):
         out =  nbsum(operands)
         self.dW = out
         self.dX = np.full((operands.shape[0], operands.shape[1]), self.value, dtype= np.float64)
-        assert self.dX.ndim == 2
+        assert self.dX.ndim == 2, self.dX.ndim
         return out

src/SymMFEA/utils/functional.py
@@ -6,14 +6,9 @@ def normalize_norm1(x):
     
     return x / (np.sum(np.abs(x)) + 1e-12)
 
-# @jit(nopython = True)
-# def normalize(x):
-#     norm2 = np.sum(x ** 2, axis = 0)
-#     norm2 = np.sqrt(norm2) + 1e-12
-#     return x / norm2
 
 @jit(nopython = True)
-def normalize(x):
+def log_normalize(x):
     margin = np.abs(x)
     margin = np.where(margin > 1, 1 + np.log(margin), margin)
     sign = np.sign(x)

test.py
@@ -16,7 +16,7 @@ from sklearn.ensemble import GradientBoostingRegressor as GBR
 from sklearn.linear_model import LinearRegression as LNR
 np.seterr(all='raise')
 
-ix = 1
+ix = 3
 Z = np.loadtxt(f"datasets/dataset_{ix}.csv", delimiter=",", skiprows=1)
 X, y = Z[:, :-1], Z[:, -1]
 # X, y = load_diabetes(return_X_y= True)