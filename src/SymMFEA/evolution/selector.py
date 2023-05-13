3a5794aa6e8f32748c561350b945dc6be361ccb2
minhnh <haiminhnguyen2001@gmail.com>
2023-05-13 22:12:50 +0700

    fix bug free space and merge tree

--- src/SymMFEA/evolution/selector.py
+++ src/SymMFEA/evolution/selector.py
@@ -10,18 +10,20 @@ class Selector():
         
     @timed
     def __call__(self, population:Population, *args, **kwds) -> List[int]:
-        ls_idx_selected = []
         for idx_subpop, subpop in enumerate(population):
             N = min(population.nb_inds_tasks[idx_subpop], len(subpop))
             l = len(subpop)
             
             #select top
             idx_selected_inds = np.arange(N).tolist()    
-            subpop.select(idx_selected_inds)
+            
             
             #free space not selected idx
-            weight_manager.WM.free_space(np.arange(N + 1, l))
+            not_selected = np.arange(N + 1, l).tolist()
+            if len(not_selected):
+                weight_manager.WM.free_space([subpop.ls_inds[i].genes.position for i in not_selected])
             
+            subpop.select(idx_selected_inds)
 
     
     
