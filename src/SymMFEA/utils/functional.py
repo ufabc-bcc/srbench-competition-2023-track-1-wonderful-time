4f7009609b9267b4af87512b0ffd1c840a730de1
minhnh <haiminhnguyen2001@gmail.com>
2023-05-20 16:02:11 +0700

    add test log-loss metric and fix bug log loss

--- src/SymMFEA/utils/functional.py
+++ src/SymMFEA/utils/functional.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numba as nb
 import os
+EPS = np.finfo(np.float64).eps
 numba_operator_wrapper = nb.njit(nb.float64[:](nb.float64[:, :]), cache= os.environ.get('DISABLE_NUMBA_CACHE') is None)
 numba_operator_with_grad_wrapper = nb.njit(nb.types.Tuple((nb.float64[:], nb.float64[:, :]))(nb.float64[:, :]), cache= os.environ.get('DISABLE_NUMBA_CACHE') is None)
 
